87b7db8be08b2e627e7411793307e15044bee7a0
7asaniix <47145655+7asaniix@users.noreply.github.com>
2019-02-04 15:20:08 +0300

    updated
    it is the updated version of main.dart it has no problems

---
+++ new.dart
@@ -0,0 +1,24 @@
+//ok
+//i dont know why i used variables to print but ok
+ 
+void main() {
+String firststring = ("password is .........");
+  int x = 5;
+    double y = 5.0;
+  String secstring = "my name is .....";
+    String trdstring = "Hasan";   
+var list = [1, 2, 3];
+  list[0];
+ bool exit =  true;
+  
+  
+  print(firststring);
+  print(x + y);
+  print(secstring);
+  print(trdstring);
+  print ("just kidding");
+  print(exit);
+  print(list[1]);
+
+
+}
